989bb1a4ce2edabfb379e237d6f7c82065d9835c
Emmanuel Gyau-Amoakoh <manuel20000@gmx.de>
2024-01-18 00:13:12 +0100

    Vorläufige Filterungslogik erstellt
    - Kommentare des Codes folgen soweit noch
    - csv-Datei als vorläufige Ausgabe mit allen gefilterten Daten
    - Filterung in Zukunft liegender Daten folgt noch (Erkennbar: Verspätung -30 oder auch mal 0
    - Verspätungenangabe ist in Sekunden, nicht wie von VBN angegeben in Minuten
    - Verspätungsdaten nicht an jeder Haltestelle gesammelt (siehe StopSequence)

--- src/data_collection/data_cleanup/public_transit/public_transit_data_cleaner.py
+++ src/data_collection/data_cleanup/public_transit/public_transit_data_cleaner.py
@@ -1,12 +1,57 @@
-from public_transit_fetch import get_gtfsr_data, extract_stop_time_updates, create_stop_time_updates_df
+import public_transit_fetch as pt_fetch
+import id_translation as transl
+import pandas as pd
+
+def remove_non_matching_stop_time_updates(stop_time_updates_df, trips_bsag_df):
+    # Inner Join zwischen stop_time_updates_df und trips_bsag_df
+    merged_df = pd.merge(stop_time_updates_df, trips_bsag_df, how='inner', left_on='TripId', right_on='trip_id')
+
+
+    return merged_df
 
 
 if __name__ == "__main__":
     gtfsr_url = "https://gtfsr.vbn.de/gtfsr_connect.json"
-    gtfsr_data = get_gtfsr_data(gtfsr_url)
+    
+    gtfsr_data = pt_fetch.get_gtfsr_data(gtfsr_url)
 
-    stop_time_updates = extract_stop_time_updates(gtfsr_data)
+    stop_time_updates = pt_fetch.extract_stop_time_updates(gtfsr_data)
 
-    stop_time_updates_df = create_stop_time_updates_df(stop_time_updates)
+    stop_time_updates_df = pt_fetch.create_stop_time_updates_df(stop_time_updates)
 
-    print(stop_time_updates_df.head())
+
+    result_dataframes = transl.process_gtfs_data()
+
+    # Zugriff auf die DataFrames
+    stops_bremen_df = result_dataframes["stops_bremen_df"]
+    agency_bsag_df = result_dataframes["agency_bsag_df"]
+    routes_bsag_df = result_dataframes["routes_bsag_df"]
+    trips_bsag_df = result_dataframes["trips_bsag_df"]
+    transfer_bsag_df = result_dataframes["transfer_bsag_df"]
+
+
+    stop_times_bsag_updates = remove_non_matching_stop_time_updates(stop_time_updates_df, trips_bsag_df)
+
+    stop_times_bsag_updates['Startzeit (Anfanghaltestelle)'] = stop_times_bsag_updates['StartTime']
+    stop_times_bsag_updates['Linie'] = stop_times_bsag_updates['route_id'].map(routes_bsag_df.set_index('route_id')['route_short_name'])
+    stop_times_bsag_updates['Haltestelle'] = stop_times_bsag_updates['StopId'].map(stops_bremen_df.set_index('stop_id')['stop_name'])
+    stop_times_bsag_updates['Richtung'] = stop_times_bsag_updates['trip_headsign']
+    stop_times_bsag_updates['Abfahrtsverspaetung in Sek.'] = stop_times_bsag_updates['DepartureDelay']
+    stop_times_bsag_updates['Ankunftsverspaetung in Sek.'] = stop_times_bsag_updates['ArrivalDelay']
+   
+    
+
+    columns_to_remove = ['TripId', 'RouteId', 'trip_id','route_id','ScheduleRelationship', 'StopId', 'ScheduleRelationshipStop', 'DepartureDelay', 'ArrivalDelay', 'trip_id', 'route_id', 'trip_headsign','trip_headsign', 'StartTime']
+    stop_times_bsag_updates.drop(columns=columns_to_remove, inplace=True)
+
+    # Reihenfolge der Spalten
+    columns_order = ['StartDate', 'Startzeit (Anfanghaltestelle)', 'Linie', 'Richtung', 'Haltestelle', 'StopSequence', 'Ankunftsverspaetung in Sek.', 'Abfahrtsverspaetung in Sek.']
+
+    # DataFrame mit neuer Spaltenreihenfolge erstellen
+    stop_times_bsag_updates = stop_times_bsag_updates[columns_order]
+
+    #print(stop_times_bsag_updates.head())
+
+    stop_times_bsag_updates.to_csv("stop_times_bsag_updates.csv", index=False)
+
+    
--- src/data_collection/data_cleanup/public_transit/public_transit_fetch.py
+++ src/data_collection/data_cleanup/public_transit/public_transit_fetch.py
@@ -20,19 +20,22 @@ def extract_stop_time_updates(gtfsr_data):
             trip_update = entity.get('TripUpdate', {})
             stop_time_update = trip_update.get('StopTimeUpdate', [])
 
-            for stop_update in stop_time_update:
-                stop_info = {
-                    "TripId": trip_update.get('Trip', {}).get('TripId'),
-                    "RouteId": trip_update.get('Trip', {}).get('RouteId'),
-                    "StartTime": trip_update.get('Trip', {}).get('StartTime'),
-                    "StartDate": trip_update.get('Trip', {}).get('StartDate'),
-                    "ScheduleRelationship": trip_update.get('Trip', {}).get('ScheduleRelationship'),
-                    "StopId": stop_update.get('StopId'),
-                    "DepartureDelay": stop_update.get('Departure', {}).get('Delay'),
-                    "ScheduleRelationshipStop": stop_update.get('ScheduleRelationship')
-                }
+            if "##VDV##COMPOSED" not in trip_update.get('Trip', {}).get('TripId', ''):
+                for stop_update in stop_time_update:
+                    stop_info = {
+                        "TripId": int(trip_update.get('Trip', {}).get('TripId')),
+                        "RouteId": int(trip_update.get('Trip', {}).get('RouteId')),
+                        "StartTime": trip_update.get('Trip', {}).get('StartTime'),
+                        "StartDate": trip_update.get('Trip', {}).get('StartDate'),
+                        "ScheduleRelationship": trip_update.get('Trip', {}).get('ScheduleRelationship'),
+                        "StopId": int(stop_update.get('StopId')),
+                        "StopSequence": stop_update.get('StopSequence'),
+                        "ArrivalDelay": stop_update.get('Arrival', {}).get('Delay',0),
+                        "DepartureDelay": int(stop_update.get('Departure', {}).get('Delay',0)),
+                        "ScheduleRelationshipStop": stop_update.get('ScheduleRelationship')
+                    }
 
-                stop_time_updates.append(stop_info)
+                    stop_time_updates.append(stop_info)
 
         return stop_time_updates
     else:
@@ -44,6 +47,3 @@ def create_stop_time_updates_df(stop_time_updates):
         return df
     else:
         print("Fehler beim Extrahieren der StopTimeUpdates.")    
-
-
-
