ef4828aa608b64b426ed966cb948de8276d4aff1
Bengt Hansen <bhansen@uni-bremen.de>
2024-02-04 16:37:11 +0100

    Seitengröße anpassen fix

--- src/React Admin Dashboard/src/pages/MyMap.jsx
+++ src/React Admin Dashboard/src/pages/MyMap.jsx
@@ -6,22 +6,20 @@ const MyMap = () => {
   const position = [53.0826, 8.8136]; // Setze die Startposition der Karte
 
   return (
-    <MapContainer
-      center={position}
-      zoom={12}
-      style={{ height: "100%", width: "300%" }}
-    >
-      {/* Füge eine OpenStreetMap-Kachel-Schicht hinzu */}
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      />
+    <main className="map-container">
+      <MapContainer center={position} zoom={12}>
+        {/* Füge eine OpenStreetMap-Kachel-Schicht hinzu */}
+        <TileLayers
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
 
-      {/* Füge einen Marker hinzu */}
-      <Marker position={position}>
-        <Popup>Ein einfacher Marker auf der OpenStreetMap-Karte.</Popup>
-      </Marker>
-    </MapContainer>
+        {/* Füge einen Marker hinzu */}
+        <Marker position={position}>
+          <Popup>Ein einfacher Marker auf der OpenStreetMap-Karte.</Popup>
+        </Marker>
+      </MapContainer>
+    </main>
   );
 };
 
