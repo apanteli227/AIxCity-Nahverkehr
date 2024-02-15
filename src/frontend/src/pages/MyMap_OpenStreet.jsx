702770b9684b015f68e42ca0953e744c693a38c5
flosrodka <floriansrodka@gmail.com>
2024-02-15 13:46:53 +0100

    Routen werden farbig gezeichnet :)

--- src/frontend/src/pages/MyMap_OpenStreet.jsx
+++ src/frontend/src/pages/MyMap_OpenStreet.jsx
@@ -1,98 +1,93 @@
-import React, { useEffect, useState } from "react";
-import { MapContainer, TileLayer, Marker, Popup, Polyline } from "react-leaflet";
-import axios from "axios";
-import "leaflet/dist/leaflet.css";
+import React, { useEffect, useState } from 'react';
+import { MapContainer, TileLayer, Marker, Polyline } from 'react-leaflet';
+import axios from 'axios';
 
-const MyMap = () => {
-  const [busStops, setBusStops] = useState([]);
-  const [tramStations, setTramStations] = useState([]);
-  const [tramRoutes, setTramRoutes] = useState([]);
-  const position = [53.0826, 8.8136]; // Setze die Startposition der Karte
+const MapComponent = () => {
+    const [tramRoutes, setTramRoutes] = useState([]);
+    const [tramStops, setTramStops] = useState([]);
+    const position = [53.0826, 8.8136]; // Setze die Startposition der Karte
 
-  useEffect(() => {
-    // Funktion zum Laden der Bushaltestellen
-    const loadBusStops = async () => {
-      try {
-        const response = await axios.get(
-          "https://overpass-api.de/api/interpreter?data=[out:json];node[highway=bus_stop](53.07,8.76,53.10,8.87);out;"
-        );
-        setBusStops(response.data.elements);
-      } catch (error) {
-        console.error("Fehler beim Laden der Bushaltestellen:", error);
-      }
-    };
-
-    // Funktion zum Laden der Tramstationen
-    const loadTramStations = async () => {
-      try {
-        const response = await axios.get(
-          "https://overpass-api.de/api/interpreter?data=[out:json];node[railway=tram_stop](53.07,8.76,53.10,8.87);out;"
-        );
-        setTramStations(response.data.elements);
-        console.log('Raw Overpass API response Tram Stations:', response.data); // Log raw response
-      } catch (error) {
-        console.error("Fehler beim Laden der Tramstationen:", error);
-      }
-    };
+    useEffect(() => {
+        fetchTramRoutes();
+    }, []);
 
     const fetchTramRoutes = async () => {
-      try {
-        const response = await axios.get(
-          "https://overpass-api.de/api/interpreter?data=[out:json][timeout:25];area(id:3600062559)->.searchArea;relation[type=route][route=tram](area.searchArea);out geom;"
-        );
-        console.log('Raw Overpass API response:', response.data); // Log raw response
-        console.log('Converted GeoJSON:', response.data.elements); // Log converted GeoJSON
-        setTramRoutes(response.data.elements);
-      } catch (error) {
-        console.error(error);
-      }
+        try {
+            const response = await axios.get(
+                "https://overpass-api.de/api/interpreter?data=[out:json][timeout:50];(relation[network=VBN][type=route][route=tram];relation[operator=BSAG][type=route][route=bus];);out geom;"
+            );
+            console.log('Raw Overpass API response:', response.data); // Log raw response
+            const tramRoutesData = response.data.elements;
+            drawTramRoutesAndStops(tramRoutesData);
+        } catch (error) {
+            console.error(error);
+        }
     };
 
-    fetchTramRoutes();
-    loadBusStops();
-    loadTramStations();
-  }, []);
+    const drawTramRoutesAndStops = (tramRoutesData) => {
+        const routes = [];
+        const stops = [];
 
+        tramRoutesData.forEach(element => {
+            if (element.type === "relation") {
+                const tags = element.tags || {};
+                const color = tags.colour || "blue"; // Verwende standardmäßig Blau, falls keine Farbe gefunden wird
+                if (tags.type === "route" && (tags.route === "tram" || tags.route === "bus")) {
+                    element.members.forEach(member => {
+                        if (member.type === "way" && member.role === "") {
+                            const routeGeometry = member.geometry || [];
+                            // Extrahiere die Farbe aus den Tags und füge sie als Eigenschaft zur Route hinzu                          
+                            routes.push({ id: element.id, geometry: routeGeometry, color: color });
+                        }
+                        else if (element.type === "node") {
+                          const stop = tramRoutesData.find(e => e.type === "node" && e.id === element.id);
+                          if (stop) {
+                              stops.push({ id: stop.id, coords: [stop.lat, stop.lon] });
+                          }
+                      }
+                    });                   
+                }                
+            } 
+        });
 
-  return (
-    <main className="map-container">
-      <MapContainer center={position} zoom={12}>
-        {/* Füge eine schlichte OpenStreetMap-Kachel-Schicht hinzu */}
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        />
+        setTramRoutes(routes);
+        console.log('Tram Routes:', routes);
+        setTramStops(stops);
+        console.log('Tram Stops:', stops);
+    };
 
-        {/* Füge eine Overlay-TileLayer nur für Straßen hinzu */}
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution="&copy; OpenStreetMap contributors"
-          maxZoom={18}
-          minZoom={12}
-          opacity={0.5} // Stelle die Opazität nach Bedarf ein
-        />
+    return (
+        <main className="map-container">
+            <MapContainer center={position} zoom={12}>
+                {/* Füge eine schlichte OpenStreetMap-Kachel-Schicht hinzu */}
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                />
 
-        {/* Füge Marker für Bushaltestellen hinzu */}
-        {busStops.map((busStop) => (
-          <Marker key={busStop.id} position={[busStop.lat, busStop.lon]}>
-            <Popup>{busStop.tags.name || "Bushaltestelle"}</Popup>
-          </Marker>
-        ))}
-
-        {/* Füge Marker für Tramstationen hinzu */}
-        {tramStations.map((tramStation) => (
-          <Marker
-            key={tramStation.id}
-            position={[tramStation.lat, tramStation.lon]}
-          >
-            <Popup>{tramStation.tags.name || "Tramstation"}</Popup>
-          </Marker>
-        ))}
-
-        
-      </MapContainer>
-    </main>
-  );
+                {/* Füge eine Overlay-TileLayer nur für Straßen hinzu */}
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution="&copy; OpenStreetMap contributors"
+                    maxZoom={18}
+                    minZoom={12}
+                    opacity={0.5} // Stelle die Opazität nach Bedarf ein
+                />
+                {tramStops.map((stop, index) => (
+                    <Marker key={`stop_${index}`} position={stop.coords}>
+                        {/* Hier kannst du weitere Marker-Details hinzufügen, z.B. Popups mit Haltestelleninformationen */}
+                    </Marker>
+                ))}
+                {tramRoutes.map((route, index) => (
+                    <Polyline
+                        key={`route_${index}`}
+                        positions={route.geometry}
+                        color={route.color || "blue"}
+                    />
+                ))}
+            </MapContainer>
+        </main>
+    );
 };
 
-export default MyMap;
+export default MapComponent;
