1ff1b6a3fbdeb1cc5071fb9ad71e6707b4e37cce
Emmanuel Gyau-Amoakoh <manuel20000@gmx.de>
2024-02-21 17:38:15 +0100

    Lock hinzugefügt für Verspätungsdaten

--- src/backend/main.py
+++ src/backend/main.py
@@ -7,53 +7,54 @@ from data_collection.events.events_data_fetch import get_events_dataframe
 from data_collection.weather.weather_data_fetch import get_weather_dataframe
 from persistence import database_controller as dbc
 
+global lock_public_transit
+lock_public_transit = False
+
 def save_transit_data(stop_times_bsag_updates):
-    print(stop_times_bsag_updates)
-    # Daten hochladen
-    logging.info("Verspätungsdaten herunterladen...")
-    time.sleep(60)
-    # single_inserts(stop_times_bsag_updates, 'public.bsag_data')
-    """Perform single inserts of the dataframe into the PostGIS table"""
-    conn = dbc.connect(dbc.param_dic)
-    logging.info("======[" + "public.bsag_data" + "]======")
-    logging.info("conn: " + conn.dsn)
-    logging.info("======[" + "public.bsag_data" + "]======")
-    for i in stop_times_bsag_updates.index:
-        vals = [stop_times_bsag_updates.at[i, col] for col in list(stop_times_bsag_updates.columns)]
-        query = """INSERT INTO public.bsag_data (start_date,current_time,daytime,weekday,holiday,starting_stop_time,line,number_of_stops,direction,number_of_building_sites,stop,stop_sequence,arrival_delay_sec,departure_delay_sec)
-                       VALUES ('%s', '%s', '%s', '%s', '%s', %s, %s, %s, '%s', '%s', '%s', '%s');""" % (
-            vals[0],
-            vals[1],
-            vals[2],
-            vals[3],
-            vals[4],
-            vals[5],
-            vals[6],
-            vals[7],
-            vals[8],
-            vals[9],
-            vals[10],
-            vals[11],
-        )
-        dbc.execute_query(conn, query)
-        # print("execute_query: " + query)
-    dbc.disconnect(conn)
+    if lock_public_transit == False:
+        print(stop_times_bsag_updates)
+        # Daten hochladen
+        global lock_public_transit
+        lock_public_transit = True
+        logging.info("Verspätungsdaten herunterladen...")
+        # single_inserts(stop_times_bsag_updates, 'public.bsag_data')
+        """Perform single inserts of the dataframe into the PostGIS table"""
+        conn = dbc.connect(dbc.param_dic)
+        for i in stop_times_bsag_updates.index:
+            vals = [stop_times_bsag_updates.at[i, col] for col in list(stop_times_bsag_updates.columns)]
+            query = """INSERT INTO public.bsag_data (start_date,"current_time",daytime,weekday,holiday,starting_stop_time,"line",number_of_stops,direction,number_of_building_sites,stop,stop_sequence,arrival_delay_sec,departure_delay_sec)
+                                    VALUES ('%s', '%s', '%s', '%s', %s, '%s', '%s', %s, '%s', %s, '%s', %s, %s, %s);""" % (
+                vals[0],
+                vals[1],
+                vals[2],
+                vals[3],
+                vals[4],
+                vals[5],
+                vals[6],
+                vals[7],
+                vals[8],
+                vals[9],
+                vals[10],
+                vals[11],
+                vals[12],
+                vals[13]
+            )
+            dbc.execute_query(conn, query)
+            # print("execute_query: " + query)
+        dbc.disconnect(conn)
+        lock_public_transit = False
 
 def save_traffic_data(traffic_data_bsag_updates):
     print(traffic_data_bsag_updates)
     # Daten hochladen
     logging.info("Verkehrsdaten herunterladen...")
-    time.sleep(60)
     # single_inserts(traffic_data_bsag_updates, 'public.traffic_data')
     """Perform single inserts of the dataframe into the PostGIS table"""
     conn = dbc.connect(dbc.param_dic)
-    logging.info("======[" + "public.traffic_data" + "]======")
-    logging.info("conn: " + conn.dsn)
-    logging.info("======[" + "public.traffic_data" + "]======")
     for i in traffic_data_bsag_updates.index:
         vals = [traffic_data_bsag_updates.at[i, col] for col in list(traffic_data_bsag_updates.columns)]
-        query = """INSERT INTO public.traffic_data (stop_name,stop_lat,stop_lon,current_time,current_date,daytime,current_speed,freeflow_Speed,average_traffic_load_percentage)
-                       VALUES ('%s', '%s', '%s', '%s', '%s', %s, '%s', '%s', %s);""" % (
+        query = """INSERT INTO public.traffic_data (stop_name,stop_lat,stop_lon,"current_time","current_date",daytime,"current_speed",freeflow_Speed,average_traffic_load_percentage)
+                       VALUES ('%s', %s, %s, '%s', '%s', '%s', %s, %s, %s);""" % (
             vals[0],
             vals[1],
             vals[2],
@@ -72,8 +73,7 @@ def save_events_data(events_bsag_updates_df):
     conn = dbc.connect(dbc.param_dic)
     for i in events_bsag_updates_df.index:
         vals = [events_bsag_updates_df.at[i, col] for col in list(events_bsag_updates_df.columns)]
-        query = """INSERT INTO public.events_data (
-                            begin_date,begin_time,end_date,end_time,event_type,event_classification) 
+        query = """INSERT INTO public.events_data (begin_date,begin_time,end_date,end_time,event_type,event_classification) 
                             VALUES ('%s', '%s', '%s', '%s', '%s', %s);""" % (
             vals[0],
             vals[1],
@@ -91,9 +91,8 @@ def save_weather_data(weather_bremen_df):
     conn = dbc.connect(dbc.param_dic)
     for i in weather_bremen_df.index:
         vals = [weather_bremen_df.at[i, col] for col in list(weather_bremen_df.columns)]
-        query = """INSERT INTO public.weather_data (
-                            city,date,time,temperature_celsius,humidity_percentage,weather_description,wind_speed_m_s,weather_warning) 
-                            VALUES ('%s', '%s', '%s', '%s', '%s', %s, '%s', %s);""" % (
+        query = """INSERT INTO public.weather_data (city,date,time,temperature_celsius,humidity_percentage,weather_description,wind_speed_m_s,weather_warning) 
+                            VALUES ('%s', '%s', '%s', %s, %s, '%s', %s, '%s');""" % (
             vals[0],
             vals[1],
             vals[2],
@@ -107,15 +106,12 @@ def save_weather_data(weather_bremen_df):
         print("execute_query: " + query)
     dbc.disconnect(conn)    
 
-# to-do: Zu Entfernen?
-#def save_weather_data(dataframe):
-#    return dataframe
-
-
 if __name__ == "__main__":
     # Schedule tasks
 
-    schedule.every(1).minutes.do(save_transit_data(get_public_transit_dataframe("https://gtfsr.vbn.de/gtfsr_connect.json")))
+    
+    schedule.every(1).minutes.do(lambda: save_transit_data(get_public_transit_dataframe("https://gtfsr.vbn.de/gtfsr_connect.json")))
+
     
     # Parameter anpassen, sodass nur um 8 Uhr, 12 Uhr, 16 Uhr und 20 Uhr die Verkehrsdaten abgerufen werden??
     schedule.every(1).minutes.do(save_traffic_data(
