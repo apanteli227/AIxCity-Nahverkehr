9eb71f6da88bce20a5138844a40980af612a74a1
Aleksandar Pantelic <apanteli@uni-bremen.de>
2024-02-28 21:13:13 +0100

    statistiken wip

--- src/frontend/src/pages/statistics/CustomStatistics.jsx
+++ src/frontend/src/pages/statistics/CustomStatistics.jsx
@@ -4,7 +4,11 @@ import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import {Autocomplete, TextField} from "@mui/material";
+import {Autocomplete, Button, TextField} from "@mui/material";
+import {DemoContainer} from '@mui/x-date-pickers/internals/demo';
+import {AdapterDayjs} from '@mui/x-date-pickers/AdapterDayjs';
+import {LocalizationProvider} from '@mui/x-date-pickers/LocalizationProvider';
+import {DateTimePicker} from '@mui/x-date-pickers/DateTimePicker';
 
 function CustomTabPanel(props) {
     const {children, value, index, ...other} = props;
@@ -42,58 +46,107 @@ function a11yProps(index) {
 export default function BasicTabs() {
     const [value, setValue] = React.useState(0);
 
+    const linien = ['Linie 1', 'Linie 2', 'Linie 3', 'Linie 4', 'Linie 5'];
+    const haltestellen = ['Haltestelle 1', 'Haltestelle 2', 'Haltestelle 3', 'Haltestelle 4', 'Haltestelle 5'];
+    const statistiken = [
+        'Ankunftsverspätung',
+        'Abfahrtsverspätung',
+        'generierte Verspätung',
+        'Verspätungsrate der Abfahrten (nach Abfahrtsverspätung)',
+        'Anzahl der Verspätungen',
+        'Anzahl der Abfahrten pro Tag',
+        'Anzahl der Abfahrtenausfälle pro Monat',
+        'Ausfallrate',
+    ];
+
     const handleChange = (event, newValue) => {
         setValue(newValue);
     };
 
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log('Statistik anzeigen');
+        const form = event.target;
+        const data = new FormData(form);
+        console.log(data);
+        //axios.post('.../statistics', data)
+    }
+
+    //todo autocomplete durch multiselect Customized hook ersetzen
+
     return (
         <>
             <h2>Benutzerdefinierte Statistiken</h2>
-            <Box sx={{width: '100%'}}>
-                <Box sx={{borderBottom: 1, borderColor: 'divider'}}>
-                    <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-                        <Tab label="Item One" {...a11yProps(0)} />
-                        <Tab label="Item Two" {...a11yProps(1)} />
-                        <Tab label="Item Three" {...a11yProps(2)} />
-                    </Tabs>
+            <form>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DemoContainer components={['DateTimePicker']}>
+                        <DateTimePicker label="Basic date time picker"/>
+                    </DemoContainer>
+                </LocalizationProvider>
+                <Box sx={{width: '100%'}}>
+                    <Box sx={{borderBottom: 1, borderColor: 'divider'}}>
+                        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+                            <Tab label="Item One" {...a11yProps(0)} />
+                            <Tab label="Item Two" {...a11yProps(1)} />
+                            <Tab label="Item Three" {...a11yProps(2)} />
+                        </Tabs>
+                    </Box>
+                    <CustomTabPanel value={value} index={0}>
+                        <h3>Linien</h3>
+                        <Autocomplete
+                            multiple
+                            id="tags-outlined"
+                            options={linien}
+                            getOptionLabel={(option) => option.title}
+                            filterSelectedOptions
+                            renderInput={(params) => (
+                                <TextField
+                                    {...params}
+                                    label="filterSelectedOptions"
+                                    placeholder="Favorites"
+                                />
+                            )}
+                        />
+                        <Autocomplete
+                            disablePortal
+                            id="combo-box-demo"
+                            options={statistiken}
+                            sx={{width: 300}}
+                            renderInput={(params) => <TextField {...params} label="Statistik"/>}
+                        />
+                    </CustomTabPanel>
+                    <CustomTabPanel value={value} index={1}>
+                        <h3>Haltestellen</h3>
+                        <Autocomplete
+                            multiple
+                            id="tags-outlined"
+                            options={haltestellen}
+                            getOptionLabel={(option) => option.title}
+                            filterSelectedOptions
+                            renderInput={(params) => (
+                                <TextField
+                                    {...params}
+                                    label="filterSelectedOptions"
+                                    placeholder="Favorites"
+                                />
+                            )}
+                        />
+                        <Autocomplete
+                            disablePortal
+                            id="combo-box-demo"
+                            options={statistiken}
+                            sx={{width: 300}}
+                            renderInput={(params) => <TextField {...params} label="Statistik"/>}
+                        />
+                    </CustomTabPanel>
+                    <CustomTabPanel value={value} index={2}>
+                        <h3>Gesamt</h3>
+                    </CustomTabPanel>
                 </Box>
-                <CustomTabPanel value={value} index={0}>
-                    <h3>Linien</h3>
-                    <Autocomplete
-                        disablePortal
-                        id="combo-box-demo"
-                        //options={linien}
-                        sx={{width: 300}}
-                        renderInput={(params) => <TextField {...params} label="Linie"/>}
-                    />
-                    <Autocomplete
-                        disablePortal
-                        id="combo-box-demo"
-                        //options={statistiken_linien}
-                        sx={{width: 300}}
-                        renderInput={(params) => <TextField {...params} label="Statistik"/>}
-                    />
-                </CustomTabPanel>
-                <CustomTabPanel value={value} index={1}>
-                    <h3>Haltestellen</h3>
-                    <Autocomplete
-                        disablePortal
-                        id="combo-box-linie"
-                        //options={haltestellen}
-                        sx={{width: 300}}
-                        renderInput={(params) => <TextField {...params} label="Haltestelle"/>}
-                    />
-                    <Autocomplete
-                        disablePortal
-                        id="combo-box-demo"
-                        //options={statistiken_haltestellen}
-                        sx={{width: 300}}
-                        renderInput={(params) => <TextField {...params} label="Statistik"/>}
-                    />
-                </CustomTabPanel>
-                <CustomTabPanel value={value} index={2}>
-                    <h3>Gesamt</h3>
-                </CustomTabPanel>
+                <Button variant="submit" onSubmit={handleSubmit}>Anzeigen</Button>
+            </form>
+            <Box>
+                <h3>Ergebnisse</h3>
             </Box>
         </>
     );
--- src/frontend/src/pages/statistics/InterestingStatistics.jsx
+++ src/frontend/src/pages/statistics/InterestingStatistics.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 import Card from "../../components/Card.jsx";
+import {
+    Bar,
+    BarChart,
+    CartesianGrid,
+    Legend,
+    Line,
+    LineChart,
+    ResponsiveContainer,
+    Tooltip,
+    XAxis,
+    YAxis
+} from "recharts";
 
 const interestingStatistics = []
+const data = [];
 
 const InterestingStatistics = () => {
     return (
@@ -13,7 +26,55 @@ const InterestingStatistics = () => {
                     return <Card key={index} statistic={statistic}/>
                 })}
             </div>
-            <div>Graph Verspätungstrends</div>
+            <div className="charts">
+                <ResponsiveContainer width="100%" height="100%">
+                    <BarChart
+                        width={500}
+                        height={300}
+                        data={data}
+                        margin={{
+                            top: 5,
+                            right: 30,
+                            left: 20,
+                            bottom: 5,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3"/>
+                        <XAxis dataKey="name"/>
+                        <YAxis/>
+                        <Tooltip/>
+                        <Legend/>
+                        <Bar dataKey="pv" fill="#8884d8"/>
+                        <Bar dataKey="uv" fill="#82ca9d"/>
+                    </BarChart>
+                </ResponsiveContainer>
+                <ResponsiveContainer width="100%" height="100%">
+                    <LineChart
+                        width={500}
+                        height={300}
+                        data={data}
+                        margin={{
+                            top: 5,
+                            right: 30,
+                            left: 20,
+                            bottom: 5,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3"/>
+                        <XAxis dataKey="name"/>
+                        <YAxis/>
+                        <Tooltip/>
+                        <Legend/>
+                        <Line
+                            type="monotone"
+                            dataKey="pv"
+                            stroke="#8884d8"
+                            activeDot={{r: 8}}
+                        />
+                        <Line type="monotone" dataKey="uv" stroke="#82ca9d"/>
+                    </LineChart>
+                </ResponsiveContainer>
+            </div>
             <h3>Ausfälle</h3>
             <div className="cards-container">
                 {interestingStatistics.map((statistic, index) => {
