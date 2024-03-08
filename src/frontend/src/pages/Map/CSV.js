a83b7a79bb27e4e09e1a10bfa6174d79feb35dd7
flosrodka <floriansrodka@gmail.com>
2024-03-08 16:19:18 +0100

    CSV-Pfad angepasst

--- src/frontend/src/pages/Map/CSV.js
+++ src/frontend/src/pages/Map/CSV.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import StopsCSV from "../../../../data_collection/data_cleanup/resources/stops_bremen.csv";
+import StopsCSV from "../../../../backend/data_collection/resources/stops_bremen.csv";
 
 export async function fetchCsvStops() {
   return await axios.get(StopsCSV);
