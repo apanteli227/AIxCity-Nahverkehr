9182d781236f1073f5fe2fbd85d09a9b8faba079
flosrodka <floriansrodka@gmail.com>
2024-03-12 20:19:29 +0100

    Task 1154 - Template etwas aufgeräumt, Routing stimmt jetzt, Icons passender gewählt

--- src/frontend/src/pages/Map/components/Routes.jsx
+++ src/frontend/src/pages/Map/components/Routes.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { Polyline, Popup } from "react-leaflet";
 import { useRouteContext } from "../store/RouteContext";
 import { fetchRoutesAndStops } from "../API";
