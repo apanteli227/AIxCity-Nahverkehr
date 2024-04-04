d4f7479644fe419aecb60f0bd540cfaa89c5a9c2
Aleksandar Pantelic <apanteli@uni-bremen.de>
2024-04-04 20:41:52 +0200

    #1156 wip

--- src/frontend/src/pages/statistics/CustomStatistics.jsx
+++ src/frontend/src/pages/statistics/CustomStatistics.jsx
@@ -8,6 +8,9 @@ import {Autocomplete, Button, Checkbox, TextField} from "@mui/material";
 import {getCustomStatistics, getLines, getStops} from "../../api.js";
 import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
 import CheckBoxIcon from '@mui/icons-material/CheckBox';
+import {DateTimePicker, LocalizationProvider} from "@mui/x-date-pickers";
+import dayjs from "dayjs";
+import {AdapterDayjs} from "@mui/x-date-pickers/AdapterDayjs";
 
 const icon = <CheckBoxOutlineBlankIcon fontSize="small"/>;
 const checkedIcon = <CheckBoxIcon fontSize="small"/>;
@@ -24,17 +27,20 @@ const statistiken = [
 ];
 
 export default function BasicTabs() {
-    const [value, setValue] = useState(0);
+    // workaround for the (initially planned) form object
+    //todo put everything in the form object that can be put there
+    const [startDateTime, setStartDateTime] = useState(null);
+    const [endDateTime, setEndDateTime] = useState(null);
+    const [selectedStatistic, setSelectedStatistic] = useState(statistiken[1]);
+    const [selectedRadio, setSelectedRadio] = useState("");
     const [lines, setLines] = useState(["1 - Bf Mahndorf", "1 - Huchting"]);
     const [stops, setStops] = useState(["Hauptbahnhof"]);
-    const [formData, setFormData] = useState({
-        mode: value === 0 ? "linien" : value === 1 ? "haltestellen" : "gesamt",
-        startDateAndTime: "",
-        endDateAndTime: "",
-        selectedItems: [],
-        selectedStatistic: statistiken[1],
-        selectedRadio: "",
-    });
+    const [tab, setTab] = useState(0);
+    const [allValues, setAllValues] = useState(tab === 0 ? lines : stops);
+    const selectedValues = React.useMemo(
+        () => allValues.filter((v) => v.selected),
+        [allValues],
+    );
 
     useEffect(
         () => {
@@ -56,12 +62,13 @@ export default function BasicTabs() {
                 .catch((error) => {
                     console.error("Error fetching stops data:", error);
                 });
-        }, [formData] // Empty dependency array ensures that the effect runs only once on component mount
+        }, [] // Empty dependency array ensures that the effect runs only once on component mount
     );
 
     const handleSubmit = (event) => {
+        console.log("Form data:", mode);
         event.preventDefault();
-        getCustomStatistics(new FormData(formData))
+        getCustomStatistics(new FormData(mode))
             .then((response) => {
                 const data = response.data;
                 console.log("Data:", data);
@@ -74,188 +81,171 @@ export default function BasicTabs() {
 
     const handleInputChangeList = (event, value) => {
         console.log(value)
-        setFormData((prevData) => ({
+        setMode((prevData) => ({
             ...prevData,
-            selectedItems: [...formData["selectedItems"], ...value],
+            selectedItems: [...mode["selectedItems"], ...value],
         }));
         console.log(value)
-        console.log({formData});
+        console.log({formData: mode});
     };
 
     const handleInputChange = (field, value) => {
         console.log(value)
-        setFormData((prevData) => ({
+        setMode((prevData) => ({
             ...prevData,
             [field]: value,
         }));
-        console.log({formData});
+        console.log({formData: mode});
     };
 
     const handleTabChange = (event, newValue) => {
-        setValue(newValue);
+        setTab(newValue);
     };
 
     return (
-        <main className="statistics-box">
+        < className="statistics-box">
             <h2>Benutzerdefinierte Statistiken</h2>
-            <form>
-                <TextField
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DateTimePicker
                     className="statistics-box-box"
-                    id="start-datetime"
-                    label="Start Date & Time"
-                    type="datetime-local"
-                    InputLabelProps={{
-                        shrink: true,
-                    }}
-                    value={formData.startDateAndTime}
-                    onChange={(e) => handleInputChange("startDateAndTime", e.target.innerText)}
+                    value={startDateTime}
+                    onChange={setStartDateTime}
+                    referenceDate={dayjs('2022-04-17T15:30')}
                 />
-                <TextField
+                <DateTimePicker
                     className="statistics-box-box"
-                    id="end-datetime"
-                    label="End Date & Time"
-                    type="datetime-local"
-                    InputLabelProps={{
-                        shrink: true,
-                    }}
-                    value={formData.endDateAndTime}
-                    onChange={(e) => handleInputChange("endDateAndTime", e.target.innerText)}
+                    value={endDateTime}
+                    onChange={setEndDateTime}
+                    referenceDate={dayjs('2022-04-17T15:30')}
                 />
-
-                <Box sx={{width: "100%"}}>
-                    <Box sx={{borderBottom: 1, borderColor: "divider"}}>
-                        <Tabs
-                            value={value}
-                            onChange={handleTabChange}
-                            aria-label="custom statistics tabs"
-                        >
-                            <Tab label="Linien"{...a11yProps(0)}/>
-                            <Tab label="Haltestellen" {...a11yProps(1)} />
-                            <Tab label="Beides" {...a11yProps(2)} />
-                        </Tabs>
-                    </Box>
-                    <CustomTabPanel value={value} index={0}>
-                        <h3>Linienbasierte Statistiken</h3>
-                        <Autocomplete
-                            className="statistics-box-box"
-                            multiple
-                            options={lines}
-                            disableCloseOnSelect
-                            getOptionLabel={(option) => option}
-                            renderOption={(props, option, {selected}) => (
-                                <li {...props}>
-                                    <Checkbox
-                                        icon={icon}
-                                        checkedIcon={checkedIcon}
-                                        style={{marginRight: 8}}
-                                        checked={selected}
-                                    />
-                                    {option}
-                                </li>
-                            )}
-                            onChange={(event, value, reason, details) => {
-                                console.log(event);
-                                console.log(formData["selectedItems"])
-                                console.log(value)
-                                console.log(reason)
-                                console.log(details)
-                                console.log([...formData["selectedItems"], ...value])
-                                const newForm = {
-                                    ...formData,
-                                    selectedItems: value,
-                                };
-                                setFormData(newForm);
-
-                                console.log({formData});
-                            }
-                            }
-                            renderInput={(params) => (
-                                <TextField {...params} label="Linien" placeholder="Linien"/>
-                            )}
-                        />
-                        <Autocomplete
-                            className="statistics-box-box"
-                            disablePortal
-                            id="combo-box-demo"
-                            options={statistiken}
-                            value={formData.selectedStatistic}
-                            onChange={(e) =>
-                                handleInputChange(e.target.innerText)
-                            }
-                            renderInput={(params) => (
-                                <TextField {...params} label="Statistik"/>
-                            )}
-                        />
-                    </CustomTabPanel>
-                    <CustomTabPanel value={value} index={1}>
-                        <h3>Haltestellenbasierte Statistiken</h3>
-                        <Autocomplete
-                            className="statistics-box-box"
-                            multiple
-                            options={stops}
-                            disableCloseOnSelect
-                            onInputChange={(e, value, reason) =>
-                                handleInputChangeList(e, value, reason)
-                            }
-                            renderOption={(props, option, {selected}) => (
-                                <li {...props}>
-                                    <Checkbox
-                                        style={{marginRight: 8}}
-                                        checked={formData.selectedItems.includes(option.title)}
-                                    />
-                                    {option.title}
-                                </li>
-                            )}
-                            style={{width: 300}}
-                            renderInput={(params) => (
-                                <TextField
-                                    {...params}
-                                    label="Haltestellen"
-                                    placeholder="Haltestellen"
-                                />
-                            )}
-                        />
-                        <Autocomplete
-                            className="statistics-box-box"
-                            disablePortal
-                            options={statistiken}
-                            onChange={(e) => {
-                                console.log(e.target);
-                                handleInputChange(e.target.innerText)
-                            }}
-                            value={formData.selectedStatistic}
-                            sx={{width: 300}}
-                            renderInput={(params) => (
-                                <TextField {...params} label="Statistik"/>
-                            )}
-                        />
-                    </CustomTabPanel>
-                    <CustomTabPanel value={value} index={2}>
-                        <h3>Linien- und haltestellenbasierte Statistiken</h3>
-                    </CustomTabPanel>
+            </LocalizationProvider>
+            <Box sx={{width: "100%"}}>
+                <Box sx={{borderBottom: 1, borderColor: "divider"}}>
+                    <Tabs
+                        value={tab}
+                        onChange={handleTabChange}
+                        aria-label="custom statistics tabs"
+                    >
+                        <Tab label="Linien"/>
+                        <Tab label="Haltestellen"/>
+                        <Tab label="Beides"/>
+                    </Tabs>
                 </Box>
-                <Button variant="submit" onSubmit={handleSubmit}>
-                    Anzeigen
-                </Button>
-            </form>
+                <CustomTabPanel value={tab} index={0}>
+                    <h3>Linienbasierte Statistiken</h3>
+                    <Autocomplete
+                        className="statistics-box-box"
+                        multiple
+                        options={lines}
+                        disableCloseOnSelect
+                        getOptionLabel={(option) => option}
+                        renderOption={(props, option, {selected}) => (
+                            <li {...props}>
+                                <Checkbox
+                                    icon={icon}
+                                    checkedIcon={checkedIcon}
+                                    style={{marginRight: 8}}
+                                    checked={selected}
+                                />
+                                {option}
+                            </li>
+                        )}
+                        onChange={(event, value, reason, details) => {
+                            const newForm = {
+                                ...mode,
+                                selectedItems: value,
+                            };
+                            setMode(newForm);
+                        }
+                        }
+                        renderInput={(params) => (
+                            <TextField {...params} label="Linien" placeholder="Linien"/>
+                        )}
+                    />
+                    <Autocomplete
+                        className="statistics-box-box"
+                        disablePortal
+                        id="combo-box-demo"
+                        options={statistiken}
+                        value={""}
+                        onChange={(e) =>
+                            handleInputChange(e.target.innerText)
+                        }
+                        renderInput={(params) => (
+                            <TextField {...params} label="Statistik"/>
+                        )}
+                    />
+                </CustomTabPanel>
+                <CustomTabPanel value={tab} index={1}>
+                    <h3>Haltestellenbasierte Statistiken</h3>
+                    <Autocomplete
+                        className="statistics-box-box"
+                        multiple
+                        options={stops}
+                        getOptionLabel={(option) => option}
+                        disableCloseOnSelect
+                        onInputChange={(e, value, reason) =>
+                            handleInputChangeList(e, value, reason)
+                        }
+                        renderOption={(props, option, {selected}) => (
+                            <li {...props}>
+                                <Checkbox
+                                    style={{marginRight: 8}}
+                                    checked={selectedItems.includes(option)}
+                                />
+                                {option}
+                            </li>
+                        )}
+                        style={{width: 300}}
+                        renderInput={(params) => (
+                            <TextField
+                                {...params}
+                                label="Haltestellen"
+                                placeholder="Haltestellen"
+                            />
+                        )}
+                    />
+                    <Autocomplete
+                        className="statistics-box-box"
+                        disablePortal
+                        options={statistiken}
+                        onChange={(e) => {
+                            console.log(e.target);
+                            handleInputChange(e.target.innerText)
+                        }}
+                        value={selectedStatistic}
+                        sx={{width: 300}}
+                        renderInput={(params) => (
+                            <TextField {...params} label="Statistik"/>
+                        )}
+                    />
+                </CustomTabPanel>
+                <CustomTabPanel value={tab} index={2}>
+                    <h3>Linien- und haltestellenbasierte Statistiken</h3>
+                </CustomTabPanel>
+            </Box>
+            <Button variant="submit"
+                    onClick={() => console.log(tab, startDateTime, endDateTime, selectedValues, selectedStatistic, selectedRadio)}>
+                Anzeigen
+            </Button>
             <Box>
                 <h3>Ergebnisse</h3>
             </Box>
-        </main>
+        </>
     );
 
     function CustomTabPanel(props) {
-        const {children, value, index, ...other} = props;
+        const {children, tab, index, ...other} = props;
 
         return (
             <div
                 role="tabpanel"
-                hidden={value !== index}
+                hidden={tab !== index}
                 id={`simple-tabpanel-${index}`}
                 aria-labelledby={`simple-tab-${index}`}
                 {...other}
             >
-                {value === index && (
+                {tab === index && (
                     <Box sx={{p: 3}}>
                         <Typography>{children}</Typography>
                     </Box>
