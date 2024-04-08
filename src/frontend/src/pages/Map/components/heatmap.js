eb72944d0e43f0ff12f4575b7d8ce26e625629af
Jasper <clemensj@uni-bremen.de>
2024-04-08 12:31:21 +0900

    Sample Data for Heatmap

---
+++ src/frontend/src/pages/Map/components/heatmap.js
@@ -0,0 +1,73 @@
+// Test Datei für Heatmap Komponente
+
+export const geojson = {
+    "type": "FeatureCollection",
+    "features": [
+   {
+     "type": "Feature",
+     "geometry": {
+        "type": "Point",
+        "coordinates":  [ 77.75659494,12.96821717 ] // todo: change the coordinates
+     },
+     "properties": {
+     "rating":5.0,
+     "Stop_name":"Hauptbahnhof"
+     }
+   },
+   {
+     "type": "Feature",
+     "geometry": {
+        "type": "Point",
+        "coordinates":  [ 77.76178066,12.98185298 ]
+     },
+     "properties": {
+     "rating":3.8,
+     "Stop_name":"Universität Zentralbereich"
+     }
+   },
+   {
+     "type": "Feature",
+     "geometry": {
+        "type": "Point",
+        "coordinates":  [ 77.680669,12.912396 ]
+     },
+     "properties": {
+     "rating":2.6,
+     "Stop_name":"Domsheide"
+     }
+   },
+   {
+     "type": "Feature",
+     "geometry": {
+        "type": "Point",
+        "coordinates":  [ 77.65278608,12.95461067 ]
+     },
+     "properties": {
+     "rating":4,
+     "Stop_name":"Rembertistraße"
+     }
+   },
+   {
+     "type": "Feature",
+     "geometry": {
+        "type": "Point",
+        "coordinates":  [ 77.64584117,13.19146155 ]
+     },
+     "properties": {
+     "rating":3,
+     "Stop_name":"Sielwall"
+     }
+   },
+   {
+     "type": "Feature",
+     "geometry": {
+        "type": "Point",
+        "coordinates":  [ 77.61589028,13.1212235 ]
+     },
+     "properties": {
+     "rating":3,
+     "Stop_name":"Test 1"
+     }
+   }
+]
+}
